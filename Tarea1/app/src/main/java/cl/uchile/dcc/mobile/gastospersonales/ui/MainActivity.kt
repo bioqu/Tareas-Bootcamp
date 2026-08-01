8181bf4532b60546cb7d7cf0b6a5eccf789651c8
Luis Pereira <61717138+bioqu@users.noreply.github.com>
2026-08-01 12:06:08 -0400

    Configuración básica de Tarea1 - Mis gastos personales para generar modelo carpetas de Modelo-Vista-Modelo de vista (MVVM)

---
+++ Tarea1/app/src/main/java/cl/uchile/dcc/mobile/gastospersonales/ui/MainActivity.kt
@@ -0,0 +1,47 @@
+package cl.uchile.dcc.mobile.gastospersonales.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cl.uchile.dcc.mobile.gastospersonales.ui.theme.GastosPersonalesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GastosPersonalesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier.Companion) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GastosPersonalesTheme {
+        Greeting("Android")
+    }
+}
